22479f0ee75abb42de7bac93c477696d1ecf832c
cfcife <aboludepeter@gmail.com>
2023-11-08 19:25:15 +0100

    Create Next App

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Component {...pageProps} />
+  <footer>
+    <small>
+      Copyright © 2023 Peter Abolude. All Rights Reserved.
+    </small>
+  </footer></>
 }
