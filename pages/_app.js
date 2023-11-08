3955a7899836a1438c64f47f655ba87c3fe0aa47
cfcife <aboludepeter@gmail.com>
2023-11-08 20:53:12 +0100

    Next Links

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,9 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <>
-  <Component {...pageProps} />
-  <footer>
-    <small>
-      Copyright © 2023 Peter Abolude. All Rights Reserved.
-    </small>
-  </footer></>
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,22 @@ import styles from "@/styles/Home.module.css";
 import Banner from "@/components/banner";
 
 export default function Home() {
+  const handleOnBannerBtnClick = () => {
+    console.log("Banner Button");
+  };
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Coffee Connoisseur</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Coffee Connoisseur</h1>
+      <div className={styles.main}>
         <Banner
           buttonText="View stores nearby"
           handleOnClick={handleOnBannerBtnClick}
         />
-      </main>
+      </div>
     </div>
   );
 }
