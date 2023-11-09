28a4055acfd36952281955be2380430b0dc58303
cfcife <aboludepeter@gmail.com>
2023-11-09 01:49:33 +0100

    Hero Image and Fonts

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,37 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Link from "next/link";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <Link
+            rel="preload"
+            href="/fonts/IBMPlexSans-Bold.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          />
+          <Link
+            rel="preload"
+            href="/fonts/IBMPlexSans-Regular.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          />
+          <Link
+            rel="preload"
+            href="/fonts/IBMPlexSans-SemiBold.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <body>
+          <Main></Main>
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
