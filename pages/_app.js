9dd5a9fd725721bfb56412d05425a51f4376970d
cfcife <aboludepeter@gmail.com>
2023-11-09 13:13:43 +0100

    using font optimization

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import '@/styles/globals.css'
+import { IBM_Plex_Sans } from "next/font/google";
+
+const IBMPlexSans = IBM_Plex_Sans({ weight: "400", subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <style jsx global>{`
+        html {
+          font-family: $(IBMPlexSans.style.fontFamily;);
+        }
+      `}</style>
       <Component {...pageProps} />
     </>
   );
