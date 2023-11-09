75cc8d973cb197eb86e54740cd53ad3cf996bba4
cfcife <aboludepeter@gmail.com>
2023-11-09 17:18:07 +0100

    Card Module

---
+++ components/card.js
@@ -0,0 +1,28 @@
+import classnames from "classnames";
+
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./card.module.css";
+
+const Card = (props) => {
+  return (
+    <Link className={styles.cardLink} href={props.href}>
+      <div className={classnames("glass", styles.container)}>
+        <div className={styles.cardHeaderWrapper}>
+          <h2 className={styles.cardHeader}>{props.name}</h2>
+        </div>
+        <div className={styles.cardImageWrapper}>
+          <Image
+            alt="coffee-store-image"
+            className={styles.cardImage}
+            src={props.image}
+            width={260}
+            height={160}
+          />
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default Card;
