23a6b47bd7ad039176b636566a34970a20ff122d
cfcife <aboludepeter@gmail.com>
2023-11-13 17:33:45 +0100

    Setup individual store page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,12 +1,104 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import coffeeStoresData from "../../data/coffee-stores.json";
+import Head from "next/head";
+import styles from "@/styles/coffee-store.module.css";
+import Image from "next/image";
+import classNames from "classnames";
 
-const CoffeeStore = () => {
+export function getStaticProps(staticProps) {
+  const params = staticProps.params;
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find((coffeeStore) => {
+        return coffeeStore.id.toString() === params.id;
+      }),
+    },
+  };
+}
+
+export function getStaticPaths() {
+  const paths = coffeeStoresData.map((coffeeStore) => {
+    return {
+      params: { id: coffeeStore.id.toString() },
+    };
+  });
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+const CoffeeStore = (props) => {
   const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  const handleUpvoteButton = () => {
+    console.log("Upvote button clicked");
+  };
+
+  const { address, name, neighbourhood, imgUrl } = props.coffeeStore;
+
   return (
-    <div>
-      Coffee Store Page: {router.query.id}
-      <Link href="/">Back to home</Link>
+    <div className={styles.layout}>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/" className={styles.link}>
+              Back to home
+            </Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            src={imgUrl}
+            alt="coffee picture"
+            width={550}
+            height={600}
+            className={styles.storeImg}
+          />
+        </div>
+        <div className={classNames("glass", styles.col2)}>
+          <div className={styles.iconWrapper}>
+            <Image
+              src="/static/icons/places.svg"
+              width="24"
+              height="24"
+              alt="Icons"
+            />
+            <p className={styles.text}>{address}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image
+              src="/static/icons/nearMe.svg"
+              width="24"
+              height="24"
+              alt="Icons"
+            />
+            <p className={styles.text}>{neighbourhood}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image
+              src="/static/icons/stars.svg"
+              width="24"
+              height="24"
+              alt="Icons"
+            />
+            <p className={styles.text}>10</p>
+          </div>
+
+          <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+            Up Vote!
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 
-import styles from "@/styles/Home.module.css";
+import styles from "@/styles/home.module.css";
 import Banner from "@/components/banner";
 import Card from "@/components/card";
 
